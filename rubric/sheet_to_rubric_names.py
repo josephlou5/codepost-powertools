79570d6ad9440ac2a02c431f2c49270b503cf98d
josephlou5 <josephlou@princeton.edu>
2021-02-18 14:20:55 -0800

    Improved process of creating comments in sheet_to_rubric_names.py

--- rubric/sheet_to_rubric_names.py
+++ rubric/sheet_to_rubric_names.py
@@ -85,15 +85,17 @@ def get_assignment_rubric(worksheet) -> dict:
 
         # get comment info
 
-        # if name does not exist, default is None
+        # if name does not exist, skip
         name = row.get(SHEET_HEADERS['name'], None)
+        if name is None or name == '':
+            continue
         # if tier does not exist, do not add it
         tier = row.get(SHEET_HEADERS['tier'], None)
         # if points does not exist, default is 0
         points = -1 * row.get(SHEET_HEADERS['point delta'], 0)
         # if text does not exist, skip
         text = row.get(SHEET_HEADERS['caption'], None)
-        if text is None:
+        if text is None or text == '':
             continue
         # if explanation does not exist, default is None
         explanation = row.get(SHEET_HEADERS['explanation'], None)
@@ -170,30 +172,6 @@ def get_all_rubric_comments(course, sheet, start_sheet=0, end_sheet=None) -> dic
 
         a_ids.remove(a_id)
 
-    # # go through the sheet and find the assignments
-    # data = dict()
-    # i = 0
-    # for i, w in enumerate(sheet.worksheets()):
-    #     worksheet = Worksheet(w)
-    #
-    #     # check assignment id in A1
-    #     try:
-    #         a_id = int(worksheet.get_cell('A1').value)
-    #     except ValueError:
-    #         continue
-    #
-    #     if a_id in a_ids:
-    #         a_name = codepost.assignment.retrieve(a_id).name
-    #
-    #         logger.debug('Getting info for "{}" assignment', a_name)
-    #         data[a_id] = get_assignment_rubric(worksheet)
-    #         logger.debug('Got all info for "{}" assignment', a_name)
-    #         a_ids.remove(a_id)
-    #
-    #         i += 1
-    #         if i == num_assignments:
-    #             break
-
     logger.info('Got all info from "{}" sheet', sheet.title)
 
     return data
@@ -269,9 +247,6 @@ def create_assignment_rubric(a_id, rubric, override_rubric=False, delete_missing
             Default is False.
     """
 
-    # TODO: allow comments to change categories. currently existing comments stay in their old category
-    #   no matter what the sheet says
-
     assignment = codepost.assignment.retrieve(a_id)
     a_name = assignment.name
 
@@ -288,31 +263,31 @@ def create_assignment_rubric(a_id, rubric, override_rubric=False, delete_missing
 
     # get all existing rubric comments
     logger.debug('Getting existing rubric comments')
-    old_comments = dict()
+    codepost_comments = dict()
     categories = dict()
     for category in assignment.rubricCategories:
         categories[category.name] = category
         for comment in category.rubricComments:
-            old_comments[comment.name] = comment
+            codepost_comments[comment.name] = comment
 
     # get all rubric comments from sheet
-    comment_categories = dict()
-    rubric_comments = dict()
+    sheet_categories = dict()
+    sheet_comments = dict()
     for category, (_, comments) in rubric.items():
         for comment in comments:
-            comment_categories[comment['name']] = category
-            rubric_comments[comment['name']] = comment
+            sheet_categories[comment['name']] = category
+            sheet_comments[comment['name']] = comment
 
     # get missing and new comment names
-    existing_names = set(old_comments.keys())
-    sheet_names = set(rubric_comments.keys())
+    existing_names = set(codepost_comments.keys())
+    sheet_names = set(sheet_comments.keys())
     missing_names = existing_names - sheet_names
-    missing_comments = [old_comments.pop(name) for name in missing_names]
+    missing_comments = [codepost_comments.pop(name) for name in missing_names]
     new_names = sheet_names - existing_names
     del existing_names, sheet_names
 
     if len(missing_comments) > 0:
-        logger.debug('Comments not in the sheet:', missing_comments)
+        logger.debug('Comments not in the sheet: ({})', ','.join(c.name for c in missing_comments))
 
         # delete rubric comments not in the sheet
         if delete_missing:
@@ -320,15 +295,19 @@ def create_assignment_rubric(a_id, rubric, override_rubric=False, delete_missing
             for comment in missing_comments:
                 # logger.debug(f'Deleting "{comment.name}" rubric comment')
                 comment.delete()
-            logger.debug('Deleted all comments not in the sheet')
+            logger.debug('Deleted {} comments', len(missing_comments))
 
             # delete old rubric categories if they have no more comments
-            logger.debug('Deleting empty categories')
+            empty_categories = list()
             for c_name in list(categories.keys()):
                 if len(categories[c_name].rubricComments) == 0:
-                    logger.debug(f'Deleting "{c_name}" rubric category')
-                    categories.pop(c_name).delete()
-            logger.debug('Deleted all empty categories')
+                    empty_categories.append(categories.pop(c_name))
+            if len(empty_categories) > 0:
+                logger.debug('Deleting empty categories')
+                for category in empty_categories:
+                    logger.debug('Deleting "{}" rubric category', category.name)
+                    category.delete()
+                logger.debug('Deleted {} empty categories', len(empty_categories))
 
     # create new categories (if needed)
     existing_categories = set(categories.keys())
@@ -337,25 +316,42 @@ def create_assignment_rubric(a_id, rubric, override_rubric=False, delete_missing
     if len(missing_categories) > 0:
         logger.debug('Creating missing rubric categories')
         for c_name in missing_categories:
-            max_points = rubric[c_name][0]
-
             logger.debug('Creating "{}" rubric category', c_name)
 
+            max_points = rubric[c_name][0]
             category = codepost.rubric_category.create(
                 name=c_name,
                 assignment=a_id,
-                pointLimit=max_points
+                pointLimit=max_points,
             )
             categories[c_name] = category
 
             logger.debug('Created "{}" rubric category', c_name)
 
+    # include category id in comment info
+    for name, comment_info in sheet_comments.items():
+        category_id = categories[sheet_categories[name]].id
+        comment_info['category'] = category_id
+
     # update existing comments
     logger.debug('Updating existing rubric comments')
-    for name, comment in old_comments.items():
-        comment_info = rubric_comments[name]
-        comment.update(id=comment.id, **comment_info)
-        # logger.debug(f'Updated {name}')
+    for name, comment in codepost_comments.items():
+        old_comment = {
+            'name': comment.name,
+            'text': comment.text,
+            'pointDelta': comment.pointDelta,
+            'category': comment.category,
+            'explanation': comment.explanation,
+            'instructionText': comment.instructionText,
+            'templateTextOn': comment.templateTextOn,
+        }
+        comment_info = sheet_comments[name]
+
+        # if anything isn't the same, update the comment
+        if old_comment != comment_info:
+            codepost.rubric_comment.update(comment.id, **comment_info)
+            logger.debug(f'Updated {name}')
+            break
 
     # create new comments
     if len(new_names) == 0:
@@ -363,10 +359,9 @@ def create_assignment_rubric(a_id, rubric, override_rubric=False, delete_missing
     else:
         logger.debug('Creating new rubric comments')
         for name in new_names:
-            c_id = categories[comment_categories[name]].id
-            comment_info = rubric_comments[name]
-            codepost.rubric_comment.create(category=c_id, **comment_info)
-            logger.debug(f'Created "{name}" in "{comment_categories[name]}"')
+            comment_info = sheet_comments[name]
+            codepost.rubric_comment.create(**comment_info)
+            logger.debug(f'Created "{name}" in "{sheet_categories[name]}"')
 
     logger.debug('Rubric creation for "{}" assignment successful', a_name)
 
